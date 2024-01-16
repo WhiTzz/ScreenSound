34f4daa0cb24f9479612776f3a71e1f9b188ae2e
WhiTzz <134019164+WhiTzz@users.noreply.github.com>
2024-01-16 17:22:30 -0300

    Add files via upload

---
+++ Filtros/LinqFilter.cs
@@ -0,0 +1,48 @@
+﻿
+using ScreenSound4.Modelos;
+using System.Linq;
+
+
+namespace ScreenSound4.Filtros
+{
+    internal class LinqFilter
+    {
+        public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+        {
+            var TodosOsGenerosMusicais = musicas.Select(genero => genero.Genero).Distinct().ToList();
+            foreach (var genero in TodosOsGenerosMusicais)
+            {
+                Console.WriteLine(genero);
+            }
+        }
+        public static void FiltrarArtistaPorGeneroMusical(List<Musica> musicas, string genero) {
+            var ArtistasPorGeneroMusical = musicas.Where(musica => musica.Genero.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+            Console.WriteLine($"Artistas do genero musical {genero}:");
+            foreach (var artist in ArtistasPorGeneroMusical)
+            {
+                Console.WriteLine(artist);
+            }
+            Console.WriteLine($"{ArtistasPorGeneroMusical.Count} Artistas tocam {genero}");
+        }
+        public static void FiltrarMusicasPorArtistas(List<Musica> musicas, string artista)
+        {
+            var MusicasDoArtista = musicas.Where(musicas => musicas.Artista!.Equals(artista)).ToList();
+            Console.WriteLine($"Musicas de {artista}:");
+            foreach (var musica in MusicasDoArtista)
+            {
+                Console.WriteLine(musica.Nome);
+            }
+            Console.WriteLine($"Existe {MusicasDoArtista.Count} musicas de {artista} no nosso banco de dados!");
+        }
+        public static void FiltrarMusicaPorKey(List<Musica> musicas)
+        {
+          
+            var MusicasFiltradasPorkey = musicas.Where(musicas=>musicas.Tonalidade.Equals("C#")).Select(musicas => musicas.Nome).Distinct().ToList();
+
+            foreach (var Musica in MusicasFiltradasPorkey)
+            {
+                Console.WriteLine($"{Musica} Tonalidade: C#");
+            }
+        }
+    }
+}
---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,25 @@
+﻿
+using ScreenSound4.Modelos;
+using System.Linq;
+
+
+
+namespace ScreenSound4.Filtros
+{
+    internal interface LinqOrder
+    {
+       
+        public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+        {
+           
+            var ArtistasOrdenados = musicas.Select(musica => musica.Artista).Distinct().OrderBy(musica => musica).ToList();
+            foreach(var Artista in ArtistasOrdenados)
+            {
+             
+                
+                Console.WriteLine($"--{Artista}");
+            }
+           
+        }
+    }
+}
