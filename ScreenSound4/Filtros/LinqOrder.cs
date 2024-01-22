b05695ffe014e350830cb00ce624d5d5d7b889b3
WhiTzz <134019164+WhiTzz@users.noreply.github.com>
2024-01-21 21:34:46 -0300

    Add files via upload

---
+++ ScreenSound4/Filtros/LinqOrder.cs
@@ -0,0 +1,37 @@
+﻿
+using ScreenSound4.Modelos;
+using System.Linq;
+
+
+
+namespace ScreenSound4.Filtros
+{
+    internal interface LinqOrder
+    {
+
+        public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+        {
+
+            var ArtistasOrdenados = musicas.Select(musica => musica.Artista).Distinct().OrderBy(musica => musica).ToList();
+            foreach (var Artista in ArtistasOrdenados)
+            {
+
+
+                Console.WriteLine($"--{Artista}");
+            }
+        }
+            public static void ExibirListaDeMusicasOrdenadas(List<Musica> musicas)
+            {
+
+                var MusicasOrdenadas = musicas.Select(musica => musica.Nome).Distinct().OrderBy(musica => musica).ToList();
+                foreach (var Musicas in MusicasOrdenadas)
+                {
+
+
+                    Console.WriteLine($"--{Musicas}");
+                }
+
+            }
+    }
+}
+
